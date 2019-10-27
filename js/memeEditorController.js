93e296a684a8f0187c810bc2e3430f49372dfbd4
Liron Shahar-Atia <liron.shaharatia@gmail.com>
2019-10-27 18:40:43 +0200

    Share option updated

--- js/memeEditorController.js
+++ js/memeEditorController.js
@@ -48,8 +48,6 @@ function renderCanvas() {
     //     // gCtx.strokeText(gMeme.lines[0],100,100);
     // }
 
-    // gCtx.strokeStyle = gMeme.color;
-    // gCtx.strokeStyle = gMeme.stroke;
     gCtx.shadowColor = "blue";
 
 
@@ -172,4 +170,22 @@ function downloadCanvas(elLink) {
 function downloadImg(elLink) {
     var imgContent = canvas.toDataURL('image/jpeg');
     elLink.href = imgContent
+}
+
+
+
+// The next 2 functions handle IMAGE UPLOADING to img tag from file system: 
+function onImgInput(ev) {
+    loadImageFromInput(ev, renderCanvas)
+}
+function loadImageFromInput(ev, onImageReady) {
+    document.querySelector('.share-container').innerHTML = ''
+    var reader = new FileReader();
+    
+    reader.onload = function (event) {
+        var img = new Image();
+        img.onload = onImageReady.bind(null, img)
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(ev.target.files[0]);
 }
